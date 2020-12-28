367c5acb64f5db80f904d178c00456a3c5911e60
Raul Hernandez <hello@raulhernandez.co.uk>
2020-12-28 17:44:07 +0800

    Update custom_js.js

--- custom_js.js
+++ custom_js.js
@@ -124,7 +124,7 @@ $(function () {
                                 console.log('Initial images loaded!.');
                                 // simulate CLICK
                                 setTimeout(function () {
-                                    document.getElementById("MenuTitle").click();
+                                    //document.getElementById("MenuTitle").click();
                                 }, 1000);
                             }
                             //
